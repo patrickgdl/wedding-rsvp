d16b60a06d5a6fd31d1ed4ad133069edc1242d3d
Patrick Lima <patrickgdlima@gmail.com>
2022-08-01 11:12:42 -0300

    feat: pure function instead of internal api call

--- pages/[id].js
+++ pages/[id].js
@@ -1,38 +1,38 @@
-import Confirm from "../components/Confirm.js";
-import When from "../components/When.js";
-import Where from "../components/Where.js";
-import Who from "../components/Who.js";
-import { server } from "../config";
+import Confirm from '../components/Confirm.js'
+import When from '../components/When.js'
+import Where from '../components/Where.js'
+import Who from '../components/Who.js'
+import { loadGuests } from '../lib/load-guests'
+import { loadGuestsAndPaths } from '../lib/load-guests-and-paths'
 
-// This function gets called at build time
+// This function gets called at build time and on the server side
 export async function getStaticPaths() {
-  const req = await fetch(`${server}/api/paths`);
-  const res = await req.json();
+  const res = await loadGuestsAndPaths()
 
-  const slugs = res?.data?.flat(); // remove empty values
+  const slugs = res?.flat() // remove empty values
 
   // Get the paths we want to pre-render based on slugs
   const paths = slugs?.map((slug) => ({
-    params: { id: slug },
-  }));
+    params: { id: slug }
+  }))
 
   // We'll pre-render only these paths at build time.
   // { fallback: false } means other routes should 404.
-  return { paths, fallback: false };
+  return { paths, fallback: false }
 }
 
 export async function getStaticProps({ params }) {
-  const req = await fetch(`${server}/api/names`);
-  const res = await req.json();
+  const res = await loadGuests()
 
   // matrix of names and paths, eg: [["Patrick Lima", "patrick-lima"], ["Juliana", "juliana"]]
-  const selectedName = res?.data?.find(([_, path]) => path === params.id);
+  // eslint-disable-next-line no-unused-vars
+  const selectedName = res?.find(([_, path]) => path === params.id)
 
   return {
     props: {
-      currentName: selectedName[0] ?? null,
-    },
-  };
+      currentName: selectedName[0] ?? null
+    }
+  }
 }
 
 export default function Home({ currentName }) {
@@ -46,5 +46,5 @@ export default function Home({ currentName }) {
         <Confirm />
       </div>
     </div>
-  );
+  )
 }
