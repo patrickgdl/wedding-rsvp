2617d60ef3cc95ba1e4e6ede8c533a40394622a2
Patrick Lima <patrickgdlima@gmail.com>
2022-08-06 18:53:17 -0300

    feat: action options

---
+++ components/Action.js
@@ -0,0 +1,69 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+import styles from '../styles/Action.module.css'
+import Check from './icons/Check'
+import Gift from './icons/Gift'
+import PigMoney from './icons/PigMoney'
+import ToastWine from './icons/ToastWine'
+
+const options = [
+  {
+    description: 'Localização do Evento',
+    newWindow: true,
+    link: 'https://g.page/chacarabaldaneventos?share',
+    Icon: ToastWine
+  },
+  {
+    description: 'Confirmar Presença',
+    link: '/confirm',
+    Icon: Check
+  },
+  {
+    description: 'Lista de Presentes',
+    newWindow: true,
+    link: 'https://lista.havan.com.br/Convidado/ItensListaPresente/631179',
+    Icon: Gift
+  },
+  {
+    description: 'Presente em Dinheiro',
+    link: '/pix',
+    Icon: PigMoney
+  }
+]
+
+const Action = () => {
+  const router = useRouter()
+
+  return (
+    <div className={styles.actions}>
+      <div className={styles.container}>
+        {options.map(({ description, link, newWindow, Icon }) => (
+          <div key={description} className={styles.wrapper}>
+            {newWindow ? (
+              <a
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button"
+              >
+                <Icon />
+              </a>
+            ) : (
+              <a
+                href={`${link}?name=${router.asPath.slice(1)}`}
+                className="button"
+              >
+                <Icon />
+              </a>
+            )}
+
+            <p className={styles.description}>{description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Action
